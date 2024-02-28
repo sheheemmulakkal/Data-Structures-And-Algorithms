5f6156baafc8f5f51fbe0f87b06b98af303aeb87
sheheemmulakkal <shaimonsheheem@gmail.com>
2024-02-28 09:54:44 +0530

    Quick sort function added

---
+++ Sort/Quick sort.js
@@ -0,0 +1,18 @@
+const quickSort = (arr) => {
+  if (arr.length < 2) return arr;
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+};
+
+console.log(
+  quickSort([5, 4, 1, 6, 8, 5, 5, 8, 5, 6, 5, 5, 6, 5, 84, 484, 8, 4])
+);
